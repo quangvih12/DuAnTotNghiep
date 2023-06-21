80400c0e1dcd26f1f9dd276aaff04246214d8fc9
dokhanh107 <ngockhanh107a@gmail.com>
2023-06-21 16:22:37 +0700

    crud size and mausac v_1

---
+++ BackEnd/src/main/java/com/example/demo/reponsitory/MauSacReponsitory.java
@@ -0,0 +1,7 @@
+package com.example.demo.reponsitory;
+
+import com.example.demo.entity.MauSac;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MauSacReponsitory extends JpaRepository<MauSac, Integer> {
+}
---
+++ BackEnd/src/main/java/com/example/demo/reponsitory/SizeReponsitory.java
@@ -0,0 +1,8 @@
+package com.example.demo.reponsitory;
+
+import com.example.demo.entity.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SizeReponsitory extends JpaRepository<Size, Integer> {
+
+}
