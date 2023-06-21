926d293fc88593c9e20c4b71cc69d3d50f5853dc
vulong305 <114568009+vulong305@users.noreply.github.com>
2023-06-21 14:56:25 +0700

    crud ChucVu and TrongLuong

---
+++ BackEnd/src/main/java/com/example/demo/reponsitory/TrongLuongRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.reponsitory;
+
+import com.example.demo.entity.ThuongHieu;
+import com.example.demo.entity.TrongLuong;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrongLuongRepository extends JpaRepository<TrongLuong, Integer> {
+    @Query("select  pot from  TrongLuong  pot where pot.donVi like :keyword or pot.ma like :keyword")
+    Page<TrongLuong> search(String keyword, Pageable pageable);
+}
