a6e8c86501f2dc9b374725f7c688ed8e5d298daf
vulong305 <114568009+vulong305@users.noreply.github.com>
2023-10-01 12:51:11 +0700

    up code màu sắc và loại

--- BackEnd/src/main/java/com/example/demo/core/Admin/repository/AdMauSacReponsitory.java
+++ BackEnd/src/main/java/com/example/demo/core/Admin/repository/AdMauSacReponsitory.java
@@ -2,9 +2,18 @@ package com.example.demo.core.Admin.repository;
 
 import com.example.demo.entity.MauSac;
 import com.example.demo.reponsitory.MauSacChiTietReponsitory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AdMauSacReponsitory extends MauSacChiTietReponsitory {
+    @Query("select pot from  MauSac pot where pot.ten like :keyword or pot.ma like :keyword")
+    Page<MauSac> search(String keyword, Pageable pageable);
+    List<MauSac> findAllByTrangThai(Integer trangThai, Sort sort);
 }
