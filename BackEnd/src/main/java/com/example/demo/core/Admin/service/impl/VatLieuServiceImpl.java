5749391e8237390c4d29f64b8fafb3c144a5669a
ngocmt111103 <ngocmtph27829@fpt.edu.vn>
2023-10-10 16:19:28 +0700

    up vật liệu và trọng lượng

--- BackEnd/src/main/java/com/example/demo/core/Admin/service/impl/VatLieuServiceImpl.java
+++ BackEnd/src/main/java/com/example/demo/core/Admin/service/impl/VatLieuServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.demo.core.Admin.service.impl;
 
-import com.example.demo.core.Admin.model.request.AdminTrongLuongRequest;
-import com.example.demo.core.Admin.repository.AdTrongLuongRepository;
-import com.example.demo.core.Admin.service.AdTrongLuongService;
-import com.example.demo.entity.TrongLuong;
+import com.example.demo.core.Admin.model.request.AdminVatLieuRequest;
+import com.example.demo.core.Admin.repository.AdVatLieuReponsitory;
+import com.example.demo.core.Admin.service.AdVatLieuServcie;
+import com.example.demo.entity.VatLieu;
 import com.example.demo.util.DataUltil;
 import com.example.demo.util.DatetimeUtil;
 import org.apache.poi.ss.usermodel.Cell;
@@ -20,98 +20,95 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
-public class TrongLuongServiceImpl implements AdTrongLuongService {
-
+public class VatLieuServiceImpl implements AdVatLieuServcie {
     @Autowired
-    private AdTrongLuongRepository repository;
+    private AdVatLieuReponsitory repository;
 
     @Override
-    public Page<TrongLuong> getAll(Integer page) {
+    public Page<VatLieu> getAll(Integer page) {
         Sort sort = Sort.by(Sort.Direction.DESC, "id");
-        Pageable pageable = PageRequest.of(page, 5 , sort);
+        Pageable pageable = PageRequest.of(page, 5, sort);
         return repository.findAll(pageable);
     }
 
     @Override
-    public List<TrongLuong> findAll() {
+    public List<VatLieu> findAll() {
         Sort sort = Sort.by(Sort.Direction.DESC, "id");
         return repository.findAll(sort);
     }
 
     @Override
-    public List<TrongLuong> getAllByTrangThai(Integer trangThai) {
+    public List<VatLieu> getAllByTrangThai(Integer trangThai) {
         Sort sort = Sort.by(Sort.Direction.DESC, "id");
         return repository.findAllByTrangThai(trangThai, sort);
     }
 
     @Override
-    public TrongLuong getById(Integer id) {
-        Optional<TrongLuong> optional = repository.findById(id);
+    public VatLieu getById(Integer id) {
+        Optional<VatLieu> optional = this.repository.findById(id);
         return optional.isPresent() ? optional.get() : null;
     }
 
     @Override
-    public Page<TrongLuong> search(String keyword, Integer page) {
-        if (keyword == null) {
-            return this.getAll(page);
-        } else {
-            Pageable pageable = PageRequest.of(page, 5);
-            return repository.search("%" + keyword + "%", pageable);
-        }
+    public Page<VatLieu> search(String keyword, Integer page) {
+        return null;
     }
 
     @Override
-    public HashMap<String, Object> add(AdminTrongLuongRequest request) {
-        request.setTrangThai(1);
-        request.setNgayTao(DatetimeUtil.getCurrentDate());
-        TrongLuong trongLuong = request.dtoToEntity(new TrongLuong());
+    public HashMap<String, Object> add(AdminVatLieuRequest dto) {
+        dto.setTrangThai(1);
+        dto.setNgayTao(DatetimeUtil.getCurrentDate());
+        VatLieu vatLieu = dto.dtoToEntity(new VatLieu());
         try {
-            TrongLuong trongLuong1 = repository.save(trongLuong);
-            trongLuong1.setMa("TL" + trongLuong1.getId());
-            return DataUltil.setData("success", repository.save(trongLuong1));
+            VatLieu vatLieu1 = repository.save(vatLieu);
+            vatLieu1.setMa("VL" + vatLieu1.getId());
+            return DataUltil.setData("success", repository.save(vatLieu1));
         } catch (Exception e) {
             return DataUltil.setData("error", "error");
         }
     }
 
     @Override
-    public HashMap<String, Object> update(AdminTrongLuongRequest request, Integer id) {
-        Optional<TrongLuong> optional = repository.findById(id);
+    public HashMap<String, Object> update(AdminVatLieuRequest dto, Integer id) {
+        Optional<VatLieu> optional = repository.findById(id);
         if (optional.isPresent()) {
-            TrongLuong tl = optional.get();
-            tl.setMa(tl.getMa());
-            tl.setDonVi(request.getDonVi());
-            tl.setTrangThai(request.getTrangThai());
-            tl.setNgayTao(request.getNgayTao());
-            tl.setNgaySua(DatetimeUtil.getCurrentDate());
-            tl.setValue(request.getValue());
+            VatLieu vatLieu = optional.get();
+            vatLieu.setMa(vatLieu.getMa());
+            vatLieu.setTen(dto.getTen());
+            vatLieu.setMoTa(dto.getMoTa());
+            vatLieu.setNgayTao(vatLieu.getNgayTao());
+            vatLieu.setNgaySua(DatetimeUtil.getCurrentDate());
             try {
-                repository.save(tl);
-                return DataUltil.setData("success", "sửa thành công");
+                return DataUltil.setData("success", repository.save(vatLieu));
             } catch (Exception e) {
                 return DataUltil.setData("error", "error");
             }
         } else {
-            return DataUltil.setData("error", "không tìm thấy loại sản phẩm để sửa");
+            return DataUltil.setData("error", "không tìm thấy vật liệu để sửa");
         }
     }
 
     @Override
     public HashMap<String, Object> delete(Integer id) {
-        Optional<TrongLuong> optional = repository.findById(id);
+        Optional<VatLieu> optional = repository.findById(id);
         if (optional.isPresent()) {
-            TrongLuong trongLuong = optional.get();
-            trongLuong.setTrangThai(0);
+            VatLieu vatLieu = optional.get();
+            vatLieu.setTrangThai(0);
             try {
-                return DataUltil.setData("success", repository.save(trongLuong));
+                return DataUltil.setData("success", repository.save(vatLieu));
             } catch (Exception e) {
                 return DataUltil.setData("error", "error");
             }
         } else {
-            return DataUltil.setData("error", "không tìm thấy trọng lượng để xóa");
+            return DataUltil.setData("error", "không tìm thấy vật liệu để xóa");
         }
     }
 
@@ -119,31 +116,32 @@ public class TrongLuongServiceImpl implements AdTrongLuongService {
     public void saveExcel(MultipartFile file) {
         if (this.isValidExcelFile(file)) {
             try {
-                List<TrongLuong> trongLuongs = this.getCustomersDataFromExcel(file.getInputStream());
-                List<TrongLuong> saveAll = this.repository.saveAll(trongLuongs);
-                for (int i = 0; i < saveAll.size(); i++) {
-                    TrongLuong tl = saveAll.get(i);
-                    tl.setMa("TL" + saveAll.get(i).getId());
-                    tl.setNgayTao(DatetimeUtil.getCurrentDate());
+                List<VatLieu> vatLieus = this.getCustomersDataFromExcel(file.getInputStream());
+                List<VatLieu> saveVatLieu = this.repository.saveAll(vatLieus);
+
+                for (int i = 0; i < vatLieus.size(); i++) {
+                    VatLieu vatLieu = vatLieus.get(i);
+                    vatLieu.setMa("VL" + saveVatLieu.get(i).getId());
+                    vatLieu.setNgayTao(DatetimeUtil.getCurrentDate());
                 }
-                this.repository.saveAll(trongLuongs);
+
+                this.repository.saveAll(vatLieus);
             } catch (IOException e) {
                 throw new IllegalArgumentException("The file is not a valid excel file");
             }
         }
+
     }
-
-
     public static boolean isValidExcelFile(MultipartFile file) {
 
         return Objects.equals(file.getContentType(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
     }
 
-    public List<TrongLuong> getCustomersDataFromExcel(InputStream inputStream) {
-        List<TrongLuong> trongLuongList = new ArrayList<>();
+    public List<VatLieu> getCustomersDataFromExcel(InputStream inputStream) {
+        List<VatLieu> vatLieuList = new ArrayList<>();
         try {
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFSheet sheet = workbook.getSheetAt(2);
 
             if (workbook != null) {
 //                System.out.println("Workbook co ton tai");
@@ -157,18 +155,19 @@ public class TrongLuongServiceImpl implements AdTrongLuongService {
                         }
                         Iterator<Cell> cellIterator = row.iterator();
                         int cellIndex = 0;
-                        TrongLuong trongLuong = new TrongLuong();
+                        VatLieu vatLieu = new VatLieu();
                         while (cellIterator.hasNext()) {
                             Cell cell = cellIterator.next();
                             switch (cellIndex) {
-                                case 0 -> trongLuong.setDonVi(cell.getStringCellValue());
-                                case 1 -> trongLuong.setTrangThai((int) cell.getNumericCellValue());
+                                case 0 -> vatLieu.setTen(cell.getStringCellValue());
+                                case 1 -> vatLieu.setTrangThai((int) cell.getNumericCellValue());
+
                                 default -> {
                                 }
                             }
                             cellIndex++;
                         }
-                        trongLuongList.add(trongLuong);
+                        vatLieuList.add(vatLieu);
                     }
                 } else {
                     System.out.println("sheet ko ton tai.");
@@ -180,6 +179,6 @@ public class TrongLuongServiceImpl implements AdTrongLuongService {
         } catch (IOException e) {
             e.getStackTrace();
         }
-        return trongLuongList;
+        return vatLieuList;
     }
 }
