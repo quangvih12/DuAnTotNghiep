b9338db2ce45d4d8545d5bdd4943fa38a72f4e8d
vinhhd198c <vinhnqph20805@fpt.edu.vn>
2023-10-11 13:02:06 +0700

    Revert "hóa đơn 2"
    This reverts commit c03aabf085759603ff88c4924e22f3682f0577be.

--- BackEnd/src/main/java/com/example/demo/infrastructure/status/HoaDonStatus.java
+++ BackEnd/src/main/java/com/example/demo/infrastructure/status/HoaDonStatus.java
@@ -3,11 +3,11 @@ package com.example.demo.infrastructure.status;
 public class HoaDonStatus {
     public static final Integer CHO_THANH_TOAN = 1;
     public static final Integer DA_HUY = 0;
-    public static final Integer YEU_CAU_XAC_NHAN = 2;               // -> dang chuan bi hang, nhan vien va khach hang co the huy hoa don
-    public static final Integer HOAN_THANH = 3;                     // -> khach hang yeu cau doi tra
-    public static final Integer DANG_CHUAN_BI_HANG = 4;             // -> giao cho don vi van chuyen, khach hang co the huy hoa don
-    public static final Integer GIAO_CHO_DON_VI_VAN_CHUYEN = 5;     // -> hoan thanh
-    public static final Integer DA_GIAO_CHO_DON_VI_VAN_CHUYEN = 6;  // xoa
+    public static final Integer YEU_CAU_XAC_NHAN = 2;
+    public static final Integer DA_XAC_NHAN = 3;
+    public static final Integer DANG_CHUAN_BI_HANG = 4;
+    public static final Integer GIAO_CHO_DON_VI_VAN_CHUYEN = 5;
+    public static final Integer DA_GIAO_CHO_DON_VI_VAN_CHUYEN = 6;
     public static final Integer YEU_CAU_DOI_TRA = 7;
     public static final Integer XAC_NHAN_DOI_TRA = 8;
 }
