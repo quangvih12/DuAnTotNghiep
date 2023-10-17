ba1676e3acd7742eef0f53bbecd8df905e077fcf
dokhanh107 <ngockhanh107a@gmail.com>
2023-10-17 23:14:14 +0700

    token jwt

--- BackEnd/src/main/java/com/example/demo/infrastructure/authentication/JwtTokenProvider.java
+++ BackEnd/src/main/java/com/example/demo/infrastructure/authentication/JwtTokenProvider.java
@@ -63,12 +63,45 @@ public class JwtTokenProvider {
             return true;
         } catch (MalformedJwtException ex) {
             throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
-        } catch (ExpiredJwtException ex) {
-            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Expired JWT token");
         } catch (UnsupportedJwtException ex) {
             throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Unsupported JWT token");
         } catch (IllegalArgumentException ex) {
             throw new BlogAPIException(HttpStatus.BAD_REQUEST, "JWT claims string is empty.");
         }
+    }
+
+
+    // refresh  JWT token
+    public boolean isTokenExpired(String accessToken){
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+            Date expirationDate = claims.getExpiration();
+
+            return expirationDate.before(new Date());
+
+        } catch (ExpiredJwtException e) {
+            return true;
+//            throw new UnauthorizedException("Token has expired. Please log in again.");
+        }
+    }
+
+    // generate JWT token
+    public String generateTokenByUser(String username){
+
+        Date currentDate = new Date();
+
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+        return token;
     }
 }
