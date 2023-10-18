88ffa411d251e128139b3cda670f4ed016409e84
Dungndph20697 <dungthoai321@gmail.com>
2023-10-18 21:56:38 +0700

    search date

--- BackEnd/src/main/java/com/example/demo/core/Admin/repository/AdHoaDonReponsitory.java
+++ BackEnd/src/main/java/com/example/demo/core/Admin/repository/AdHoaDonReponsitory.java
@@ -90,6 +90,31 @@ public interface AdHoaDonReponsitory extends HoaDonReponsitory {
             """, nativeQuery = true)
     List<AdminHoaDonResponse> getHoaDonByDate(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate,@Param("comboBoxValue") String comboBoxValue);
 
+    @Query(value = """
+                   SELECT ROW_NUMBER() OVER(ORDER BY hd.id DESC) AS stt,
+                           u.email as email ,u.sdt,hd.hinh_thuc_giao_hang as hinhThucGiaoHang, hd.ly_do as lyDo,
+                           hd.ma as maHD, u.ten as nguoiTao, hd.ngay_nhan as ngayNhan, hd.ngay_ship as ngayShip, hd.ngay_tao as ngayTao,\s
+                           hd.ngay_sua as ngaySua, hd.ten_nguoi_nhan as tenNguoiNhan, hd.tien_sau_khi_giam_gia as tienSauKhiGiam,
+                           hd.tien_ship as tienShip, hd.tong_tien as tongTien, hd.trang_thai as trangThai,
+                           hd.id as idHD,\s
+                           dc.dia_chi as diaChiCuThe, dc.id_tinh_thanh as idTinhThanh,\s
+                           dc.ten_tinh_thanh as tenTinhThanh, dc.id_quan_huyen as idQuanHuyen, dc.ten_quan_huyen as tenQuanHuyen,\s
+                           dc.id_phuong_xa as idPhuongXa, dc.ten_phuong_xa as tenPhuongXa,
+                           pttt.ten as tenPTTT, hd.ngay_thanh_toan  as ngayThanhToan
+                    FROM  datn.hoa_don hd    join datn.dia_chi dc on dc.id = hd.id_dia_chi_sdt\s
+                                             join datn.phuong_thuc_thanh_toan pttt on pttt.id = hd.id_phuong_thuc_thanh_toan\s
+                                             join datn.user u on u.id = hd.id_user
+            	   where hd.trang_thai = :trangThai and (CASE
+                                   WHEN :comboBoxValue = 'ngayTao' THEN  hd.ngay_tao >= :startDate and hd.ngay_tao <= :endDate
+                                   WHEN :comboBoxValue = 'ngaySua' THEN  hd.ngay_sua >= :startDate and hd.ngay_sua <= :endDate
+                                   WHEN :comboBoxValue = 'ngayThanhToan' THEN  hd.ngay_thanh_toan >= :startDate and hd.ngay_thanh_toan <= :endDate
+                                   WHEN :comboBoxValue = 'ngayShip' THEN  hd.ngay_ship >= :startDate and hd.ngay_ship <= :endDate
+                                   WHEN :comboBoxValue = 'ngayNhan' THEN  hd.ngay_nhan >= :startDate and hd.ngay_nhan <= :endDate
+                                   END)  
+            	  
+            """, nativeQuery = true)
+    List<AdminHoaDonResponse> getHoaDonByDateByTrangThai(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate,@Param("comboBoxValue") String comboBoxValue,@Param("trangThai") Integer trangThai);
+
 
     @Query(value = """
                      SELECT ROW_NUMBER() OVER(ORDER BY hd.id DESC) AS stt,
