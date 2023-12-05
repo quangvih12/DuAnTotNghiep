63a6b38720d4b0431319d412ad54dcf7ec7341ce
nguyenntt37 <nguyenntt37@gmail.com>
2023-12-06 00:32:06 +0700

    up code chat

--- BackEnd/src/main/java/com/example/demo/infrastructure/authentication/JwtTokenProvider.java
+++ BackEnd/src/main/java/com/example/demo/infrastructure/authentication/JwtTokenProvider.java
@@ -1,9 +1,13 @@
 package com.example.demo.infrastructure.authentication;
 
+import com.example.demo.entity.User;
 import com.example.demo.infrastructure.exception.BlogAPIException;
+import com.example.demo.reponsitory.UserReponsitory;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -19,27 +23,31 @@ public class JwtTokenProvider {
     @Value("${app-jwt-expiration-milliseconds}")
     private long jwtExpirationDate;
 
+    UserReponsitory userRepository;
+
+    public JwtTokenProvider(@Qualifier("adUserRepository") UserReponsitory userRepository) {
+        this.userRepository = userRepository;
+    }
+
     // generate JWT token
     public String generateToken(Authentication authentication){
         String username = authentication.getName();
-
+        User user = userRepository.findByEmail(authentication.getName()).orElse(null);
         Date currentDate = new Date();
-
         Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
-
         String token = Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(expireDate)
+                .claim("id", user != null ? user.getId() : null)
+                .claim("role", user != null ? user.getRole() : null)
                 .signWith(key())
                 .compact();
         return token;
     }
 
     private Key key(){
-        return Keys.hmacShaKeyFor(
-                Decoders.BASE64.decode(jwtSecret)
-        );
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
     // get username from Jwt token
@@ -91,15 +99,15 @@ public class JwtTokenProvider {
 
     // generate JWT token
     public String generateTokenByUser(String username){
-
+        User user = userRepository.findByEmail(username).orElse(null);
         Date currentDate = new Date();
-
         Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
-
         String token = Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(expireDate)
+                .claim("id", user != null ? user.getId() : null)
+                .claim("role", user != null ? user.getRole() : null)
                 .signWith(key())
                 .compact();
         return token;
